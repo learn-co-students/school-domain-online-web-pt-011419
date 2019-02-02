8b832553630fd0e5fe7cbc97fec64d1225c43c40
Max Brooks <maxbrooks114@gmail.com>
2019-02-02 18:52:19 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+require "pry"
+
+class School
+  
+  
+  
+  attr_accessor  :roster
+  attr_reader :name
+
+  def initialize (name)
+    @name=name
+    @roster= {}
+   
+  end
+
+
+  def add_student (kid, grades)
+    roster[grades] ||= []
+    roster[grades]<< kid
+  end
+  
+  def grade (grades)
+    roster[grades]
+  end
+  
+  def sort 
+  nu_hash = {}
+  roster.each do |grades, kid| 
+    nu_hash[grades] = kid.sort 
+  end 
+  nu_hash
+  end
+  
+
+end
